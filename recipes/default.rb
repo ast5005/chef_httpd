cbcc2453a4a0b8803b9abc19b11aa2603160eb12
astazebay <abdullahtazebay@gmail.com>
2021-02-18 09:10:41 -0600

    inital upload

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,27 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+# case node[:platform]
+#     when 'ubuntu'
+#     package 'apache2' do
+#         action :install
+#     end
+# end
+
+# case node[:platform]
+#     when 'redhat', 'amazon', 'centos'
+#     service 'httpd' do
+#         action [:start, :enable]
+#     end
+# end
+
+case node[:platform]
+    when 'windows'
+        File ‘ c:\user\Dimple\Desktop\test.txt’ do
+            Content ‘This is a test file”
+            action :create
+        end
+    end
+
+
+end
